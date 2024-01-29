4b0438d77c26369dc6d45bc8f43638af7ea7f005
Aftab7767 <araza14021996@gmail.com>
2024-01-29 18:19:37 +0530

    Parameter

---
+++ src/test/java/jenkins/TestScriptParamrterTest.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class TestScriptParamrterTest {
+@Test
+public void scriptTest()
+{
+	String browser=System.getProperty("browser");
+	String env = System.getProperty("env");
+}
+}
